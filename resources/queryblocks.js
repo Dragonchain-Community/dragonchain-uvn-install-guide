07ec922f2b4fec26aa318139a71e9d7661f367b0
White Raven Technology <john@whiteraventechnology.com>
2019-09-25 23:06:25 -0500

    Update queryblocks.js

--- resources/queryblocks.js
+++ resources/queryblocks.js
@@ -3,7 +3,7 @@ const sdk = require('dragonchain-sdk')
 
 const main = async () => {
     const client = await sdk.createClient()
-    const blocks = await client.queryBlocks()
+    const blocks = await client.queryBlocks({redisearchQuery:'*', limit:10, sortBy: "block_id", sortAscending: false})
 
     console.log(util.inspect(blocks, false, null, true));
 
